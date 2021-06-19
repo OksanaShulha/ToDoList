cddaa284f9c4c9c55a6e42ffb959e590b0cd05ee
OksanaShulha <shushullja@gmail.com>
2021-06-19 23:38:40 +0200

    Fixing errors

--- src/procedures/procedures.js
+++ src/procedures/procedures.js
@@ -1,14 +1,14 @@
 
-const addTodo = (event) => {
+// const addTodo = (event) => {
 
 const addTaskBtn = document.getElementById('add-task-btn');  //user add any task in the list
 const descTaskInput = document.getElementById('description-task'); //user type any task in input
-const todosWrapper = document.querySelector('.todos-wrapper'); // any task goes here
+const todoWrapper = document.querySelector('.todo-wrapper'); // any task goes here
 
 let tasks; 
-!localStorage.tasks ? tasks = [] : tasks = JSON.parse(localStorage.getItem('tasks'));
+// !localStorage.tasks ? tasks = [] : tasks = JSON.parse(localStorage.getItem('tasks'));
 
-let todoItemElems = [];
+let todoItemElem = [];
 
 
 // text from input goes here
@@ -17,7 +17,7 @@ function Task(description) {
     this.complete = false; // user is not available to change something
 }
 
-// this tamplate goes to task-wrapper
+// this template goes to task-wrapper
 const createTemplate = (task,index) => { // `` it is a dynamic strings + ${} (to repeat each time: completed and description)
     return `
     <div class="todo-item ${task.completed ? 'checked' : ''}">
@@ -37,13 +37,13 @@ const filterTasks = () => {
 }
 
 const fillHtmlList = () => {
-    todosWrapper.innerHTML = "";
-    if(task.length > 0) {
+    todoWrapper.innerHTML = '';
+    if(tasks.length > 0) {
         filterTasks();
         tasks.forEach((item,index) => {
-            todosWrapper.innerHTML += createTemplate(item, index);
+            todoWrapper.innerHTML += createTemplate(item, index);
         });
-        todoItemElems = document.querySelectorAll('.todo-item');
+        todoItemElem = document.querySelectorAll('.todo-item');
     }
 }
 
@@ -53,12 +53,20 @@ const updateLocal = () => {
     localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
+
+addTaskBtn.addEventListener('click', () => {
+    tasks.push(new Task(descTaskInput.value));
+    updateLocal();
+    fillHtmlList();
+    descTaskInput.value = '';
+})
+
 const completeTask = index => {
     tasks[index].completed = !tasks[index].completed;
     if(tasks[index].completed) {
-        todoItemElems[index].classList.add('checked');
+        todoItemElem[index].classList.add('checked');
     } else {
-        todoItemElems[index].classList.remove('checked');
+        todoItemElem[index].classList.remove('checked');
     }
     updateLocal();
     fillHtmlList();
@@ -66,7 +74,7 @@ const completeTask = index => {
 }
 
 const deleteTask = index => {
-    todoItemElems[index].classList.add('delition');
+    todoItemElem[index].classList.add('deleting');
     tasks.splice(index, 1);
     updateLocal();
     fillHtmlList;
@@ -77,6 +85,6 @@ const deleteTask = index => {
     },500)
 }
 
-};
+// };
 
-export { addTodo };
+// export { addTodo };
